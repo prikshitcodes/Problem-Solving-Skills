cafd7d72d21cce04caa7b5eae228f76b3198fc7e
Prikshit Sharma <prikshitcodes@gmail.com>
2025-04-22 10:38:47 +0530

    Create linearSearch.cpp

---
+++ Arrays/linearSearch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    // Function to find element in sorted array
+    // arr: input array
+    // k: element to be searched
+    bool searchInSorted(vector<int>& arr, int k) {
+
+        // Your code here
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]==k){
+                return true;
+            }
+        }
+        return false;
+    }
+};
